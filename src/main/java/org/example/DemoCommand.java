b5379bc725c265158abc945e6df5d2268cfb0a91
Filipe Abreu Bezerra <>
2023-11-26 20:45:03 -0300

    padrão command.

---
+++ src/main/java/org/example/DemoCommand.java
@@ -0,0 +1,10 @@
+package org.example;
+
+import org.example.command.Editor;
+
+public class DemoCommand {
+    public static void main(String[] args) {
+        Editor editor = new Editor();
+        editor.init();
+    }
+}
